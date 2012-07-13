858ec62140c99e63b87402990e94f6a29f2b3594
Chris Hilton <chris@dctank.com>
2012-07-12 22:42:00 -0600

    Added setup for Jenkins box

--- jenkins-box/src/vagrant/manifests/default.pp
+++ jenkins-box/src/vagrant/manifests/default.pp
@@ -1,2 +1,34 @@
+class jenkins {
 
+    $key_url = "http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key"
+    $repo_url = "deb http://pkg.jenkins-ci.org/debian binary/"
+    $apt_sources = "/etc/apt/sources.list"
 
+    exec { "install jenkins key":
+        command     => "wget -q -O - ${key_url} | apt-key add -; echo '${repo_url}' >> ${apt_sources}",
+        onlyif      => "grep -Fvxq '${repo_url}' ${apt_sources}",
+        path        => ["/bin", "/usr/bin"],
+    }
+
+    exec { "jenkins-apt-update":
+        command => "/usr/bin/aptitude -y update",
+        require => Exec["install jenkins key"],
+    }
+
+    package { "jenkins":
+        ensure      => present,
+        provider    => "aptitude",
+        require     => Exec["jenkins-apt-update"],
+    }
+
+    service { "jenkins":
+        enable      => true,
+        ensure      => running,
+        hasrestart  => true,
+        hasstatus   => true,
+        require     => Package["jenkins"],
+    }
+
+}
+
+class { 'jenkins': }
